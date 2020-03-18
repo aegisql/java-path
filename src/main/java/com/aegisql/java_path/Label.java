e4887e07b3099458d7f66395d0a02b7ceae30ff8
aegisql <mvteplitsky@yahoo.com>
2020-03-18 15:38:26 -0400

    added annotations

---
+++ src/main/java/com/aegisql/java_path/Label.java
@@ -0,0 +1,20 @@
+package com.aegisql.java_path;
+
+import java.lang.annotation.*;
+
+/**
+ * The Interface Label.
+ * Allows method or field match on multiple label names
+ */
+@Documented
+@Target({ElementType.METHOD,ElementType.FIELD})
+@Retention(RetentionPolicy.RUNTIME)
+public @interface Label {
+	
+	/**
+	 * Value. - collection of matching labels
+	 *
+	 * @return the string[]
+	 */
+	String[] value();
+}
---
+++ src/main/java/com/aegisql/java_path/NoLabel.java
@@ -0,0 +1,13 @@
+package com.aegisql.java_path;
+
+import java.lang.annotation.*;
+
+/**
+ * The Interface NoLabel.
+ * Exclude method from label matching, if multiple choices available
+ */
+@Documented
+@Target({ElementType.METHOD})
+@Retention(RetentionPolicy.RUNTIME)
+public @interface NoLabel {
+}

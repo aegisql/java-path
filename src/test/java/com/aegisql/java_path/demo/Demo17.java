8e02b6c410db87f534e8129ec23db75697a79d26
aegisql <mvteplitsky@yahoo.com>
2020-04-08 12:52:33 -0400

    save

---
+++ src/test/java/com/aegisql/java_path/demo/Demo17.java
@@ -0,0 +1,53 @@
+package com.aegisql.java_path.demo;
+
+import com.aegisql.java_path.PathUtils;
+import org.junit.Test;
+
+import static junit.framework.TestCase.assertEquals;
+
+public class Demo17 {
+
+    public static class A {
+        String name;
+
+        public void setName(String name) {
+            this.name = name;
+        }
+    }
+
+    public static class B {
+        A a;
+
+        public A getA() {
+            if(a==null) {
+                a = new A();
+            }
+            return a;
+        }
+    }
+
+    @Test
+    public void test() {
+        B b = new B();
+        PathUtils pathUtils = new PathUtils(B.class);
+        pathUtils.applyValueToPath("getA().setName($)", b, "John");
+        assertEquals("John",b.a.name);
+    }
+
+    @Test
+    public void testWithOptionalDollarSign() {
+        B b = new B();
+        PathUtils pathUtils = new PathUtils(B.class);
+        pathUtils.applyValueToPath("getA().setName()", b, "John");
+        assertEquals("John",b.a.name);
+    }
+
+    @Test
+    public void testWithOptionalDollarSignAndParenthesis() {
+        B b = new B();
+        PathUtils pathUtils = new PathUtils(B.class);
+        pathUtils.applyValueToPath("getA().setName", b, "John");
+        assertEquals("John",b.a.name);
+    }
+
+}

98a4d02416db2cac9813f3040761540574a917e7
aegisql <mvteplitsky@yahoo.com>
2020-05-05 17:15:39 -0400

    save

---
+++ src/test/java/com/aegisql/java_path/demo/Demo25.java
@@ -0,0 +1,38 @@
+package com.aegisql.java_path.demo;
+
+import com.aegisql.java_path.JavaPath;
+import org.junit.Test;
+
+import java.util.HashMap;
+import java.util.concurrent.atomic.AtomicInteger;
+import java.util.function.Supplier;
+
+import static junit.framework.TestCase.assertEquals;
+
+public class Demo25 {
+
+    public static class A {
+        HashMap<String,Object> map;
+    }
+
+    @Test
+    public void test() {
+        A a = new A();
+        Supplier<AtomicInteger> supplier = AtomicInteger::new;
+
+        JavaPath pathUtils = new JavaPath(A.class);
+        pathUtils.evalPath("map.put(name)", a, "John");
+        pathUtils.evalPath("map.put(msg)", a, new StringBuilder());
+
+        pathUtils.evalPath("map.get(msg).append('Dear ').append(#1.get(name)).append", a, "!");
+        pathUtils.evalPath("map.get(counter)?put(counter,$1.get).getAndAdd", a, 100,supplier);
+        pathUtils.evalPath("map.get(counter)?put(counter,$1.get).getAndAdd", a, 100,supplier);
+
+        assertEquals("John",a.map.get("name"));
+        assertEquals("Dear John!",a.map.get("msg").toString());
+        assertEquals("200",a.map.get("counter").toString());
+
+        System.out.println(a.map);
+    }
+
+}

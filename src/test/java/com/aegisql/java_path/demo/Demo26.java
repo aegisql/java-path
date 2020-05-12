9618a63344181f9ebe044869c403e8031e0c07f9
aegisql <mvteplitsky@yahoo.com>
2020-05-12 15:39:47 -0400

    save

--- src/test/java/com/aegisql/java_path/demo/Demo26.java
+++ src/test/java/com/aegisql/java_path/demo/Demo26.java
@@ -10,15 +10,17 @@ public class Demo26 {
     public static class A {
         String firstName;
         String lastName;
+        int age;
     }
 
     @Test
     public void test() {
         A a = new A();
         JavaPath pathUtils = new JavaPath(A.class);
-        pathUtils.evalPath("firstName;lastName;", a, "John","Smith");
+        pathUtils.evalPath("firstName; lastName; age", a, "John","Smith",55);
         assertEquals("John",a.firstName);
         assertEquals("Smith",a.lastName);
+        assertEquals(55,a.age);
     }
 
 }

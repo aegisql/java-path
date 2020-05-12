aa03dd5ac1302a0e4b044e80250844623c7ea415
aegisql <mvteplitsky@yahoo.com>
2020-05-12 16:33:52 -0400

    save

--- src/test/java/com/aegisql/java_path/demo/Demo26.java
+++ src/test/java/com/aegisql/java_path/demo/Demo26.java
@@ -41,4 +41,14 @@ public class Demo26 {
         assertEquals(55,a.age);
     }
 
+    @Test
+    public void testWithInlineValues() {
+        A a = new A();
+        JavaPath pathUtils = new JavaPath(A.class);
+        pathUtils.evalPath("firstName(John); lastName(Smith); age(int 55)", a);
+        assertEquals("John",a.firstName);
+        assertEquals("Smith",a.lastName);
+        assertEquals(55,a.age);
+    }
+
 }
